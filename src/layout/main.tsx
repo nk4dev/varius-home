2297a0d86bfb35d5ec5b5eb9aac0ea629edf4856
nknighta <nknighta@varius.technology>
2025-10-01 21:19:46 +0900

    style: 💄 add header image

--- src/layout/main.tsx
+++ src/layout/main.tsx
@@ -3,18 +3,21 @@ import Head from "next/head";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     const year = new Date().getFullYear();
-    function HeadMeta () {
-	    return (
-		    <Head>
-			<title>VARIUS</title>
-		    </Head>
-	    )
+    function HeadMeta() {
+        return (
+            <Head>
+                <title>VARIUS</title>
+            </Head>
+        )
     }
     return (
         <div>
-		<HeadMeta/>
+            <HeadMeta />
             <header>
-                <div className="bg-[#000021] text-white h-45 w-45 fixed flex items-center justify-center">
+                <svg width="24" height="24" viewBox="0 0 24 24" className="-top-50 -left-40 h-1/2 w-1/2 mr-2 fixed transform rotate-80" xmlns="http://www.w3.org/2000/svg" fill="none">
+                    <polygon points="11,0.4 20,20 2,50" fill="#fff" />
+                </svg>
+                <div className="text-[#000021] h-25 w-45 fixed flex items-center justify-center">
                     <div className="font-bold text-4xl">
                         VARIUS
                     </div>
@@ -31,15 +34,15 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                     <Link href={"https://nknighta.me"} className="text-sm text-gray-500 dark:text-gray-400 hover:underline ml-2">
                         nknighta.me
                     </Link>
-                    
+
                     <Link href={"/"} className="text-sm text-gray-500 dark:text-gray-400 hover:underline ml-2">
                         varius.technology
                     </Link>
-                    
+
                     <Link href={"https://nknighta.me/g"} className="text-sm text-gray-500 dark:text-gray-400 hover:underline ml-2">
                         Owner GitHub
                     </Link>
-                    
+
                     <Link href={"https://nknighta.me/x"} className="text-sm text-gray-500 dark:text-gray-400 hover:underline ml-2">
                         Owner X (Japanese)
                     </Link>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,12 +13,12 @@ export default function Home() {
             </h1>
         </MotionWrapper>
         <MotionWrapper s={1} style="flex flex-col bg-[#141066] h-50 w-full lg:w-1/2 md:w-2/3 sm:w-2/3 border-solid border-2 border-[#7856ff] rounded-lg flex items-start justify-center p-2">
-          <h1 className="text-4xl font-bold">VX</h1>
+          <h1 className="text-4xl font-bold">VX3 | VX</h1>
           <Link className="text-[#7856ff] px-1 underline underline-offset-4  decoration-[#7856ff]" href="https://vx.varius.technology">
             docs
             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 inline-block" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12 19l7-7-7-7m7 7H3" /></svg>
           </Link>
-          <Link className="text-[#7856ff] px-1 underline underline-offset-4  decoration-[#7856ff]" href="https://github.com/nknighta/vx">
+          <Link className="text-[#7856ff] px-1 underline underline-offset-4  decoration-[#7856ff]" href="https://github.com/nknighta/vx3">
             repository
             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 inline-block" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12 19l7-7-7-7m7 7H3" /></svg>
           </Link>
